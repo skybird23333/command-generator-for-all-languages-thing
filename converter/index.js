b7b7058a1b8b60ee75a577ce7ae2f382ad22768c
skybird23333 <2603003199a@gmail.com>
2020-08-25 16:14:18 +0800

    updated converter

--- converter/index.js
+++ converter/index.js
@@ -1,27 +1,45 @@
 const fs = require('fs');
+const { resolve } = require('path');
+const { rejects } = require('assert');
 console.log('Reading data...');
 const textFiles = fs.readdirSync('./texts').filter(file => file.endsWith('.lang'));
 
-const minutes = 2
 var data = {}
+var promisesArray = []
 
-textFiles.forEach(async file => {
+textFiles.forEach(function (file, i) {
     data[file] = {}
-    fs.readFile(`./texts/${file}`,"utf8", async function(err, text){
-        const texts = text.split('\n')
-        texts.forEach(async line => {
-            if(!(line.startsWith('#') || line === "\n" || line === "\n ")) return
-            data[file][line.replace('	#', '').split('=')[0]] = line.replace('   #', '').split('=')[1]
-        })
-    })
-})
+    let x = new Promise((resolve, reject) => {
+        fs.readFile(`./texts/${file}`, "utf8", function (err, text) {
+            if (err) console.error(err)
+            console.log(`Parsing ${file}`)
+            let texts = text.split('\n')
 
-setTimeout(function () {
-    const filedata = JSON.stringify(data, null, "\t")
+            let y = new Promise((resolve, reject) => {
+                texts.forEach(function (line, x) {
+                    if (!(line.startsWith('#') || line === "\n" || line === "\n ")) {
+                        data[file][line.replace('    #', '').split('=')[0]] = line.replace('   #', '').split('=')[1]
+                    }
+                })
+                resolve()
+            }).then(z => resolve())
+        })
+
+    })
+    promisesArray.push(x)
+
+});
+
+Promise.all(promisesArray).then(() => {
+    let filedata = JSON.stringify(data, null, "\t")
     fs.writeFile('data.json', filedata, function (err) {
         if (err) {
             console.log(err)
         }
-        else console.log('kdone')
+        else console.log('done')
     })
-}, 1000 * 60 * minutes)
+});
+
+
+//made by 18.58.9.9.26#2436 from discord
+//i obviously cant write code that good lmao
